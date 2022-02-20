3b23a0638182e1315b71e26288e28c1798ab4869
olciaczekswiraczek <61986873+olciaczekswiraczek@users.noreply.github.com>
2022-02-20 18:17:45 +0100

    skyBox implemented

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,7 @@ void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 unsigned int loadTexture(const char* path);
+unsigned int loadCubeMap(std::vector<std::string> faces);
 
 unsigned int generateInstanceVBO(unsigned int amount, glm::mat4* matrices, Model* loadedModel) {
     unsigned int buffer;
@@ -140,8 +141,8 @@ int main()
     std::shared_ptr<Shader> vertexShader(new Shader("shader.vert", VERTEX_SHADER));
     std::shared_ptr<Shader> fragmentShader(new Shader("shader.frag", FRAGMENT_SHADER));
 
-    std::shared_ptr<Shader> instancedVertexShader(new Shader("drawInstanced.vert", VERTEX_SHADER));
-    std::shared_ptr<Shader> instancedFragmentShader(new Shader("drawInstanced.frag", FRAGMENT_SHADER));
+    std::shared_ptr<Shader> skyboxVertexShader(new Shader("skybox.vert", VERTEX_SHADER));
+    std::shared_ptr<Shader> skyboxFragmentShader(new Shader("skybox.frag", FRAGMENT_SHADER));
 
     std::shared_ptr<Shader> lightingVertexShader(new Shader("light.vert", VERTEX_SHADER));
     std::shared_ptr<Shader> lightingFragmentShader(new Shader("light.frag", FRAGMENT_SHADER));
@@ -150,7 +151,7 @@ int main()
     std::shared_ptr<Shader> lightCubeFragmentShader(new Shader("lightCube.frag", FRAGMENT_SHADER));
 
     ShaderProgram shaderProgram(vertexShader, fragmentShader);
-    ShaderProgram instancedShaderProgram(instancedVertexShader, instancedFragmentShader);
+    ShaderProgram skyboxShaderProgram(skyboxVertexShader, skyboxFragmentShader);
     ShaderProgram lightingShaderProgram(lightingVertexShader, lightingFragmentShader);
     ShaderProgram lightCubeShaderProgram(lightCubeVertexShader, lightCubeFragmentShader);
 
@@ -223,22 +224,6 @@ int main()
              roofNode->Translate(0, 1.0, 0);
              roofNode->Scale(glm::vec3(1.2, 1.0, 1.2));
 
-                 /*  model = glm::translate(model, glm::vec3(x, 0.0f, z));
-                   modelRoof = glm::translate(modelRoof, glm::vec3(x, 1.0f, z));
-                   float scale = 0.2;
-                   //model = glm::scale(model, glm::vec3(scale));
-
-                   modelMatrices[i * dim + j] = model;
-                   roofMatrices[i * dim + j] = modelRoof;
-                   wallsGraphNode->setTransform(&modelMatrices[i * dim + j]);
-                   roofGraphNode->setTransform(&roofMatrices[i * dim + j]);
-
-                   wallsGraphNode->addChild(roofGraphNode);
-
-                   wallsGraphNodes.push_back(wallsGraphNode);
-                   roofGraphNodes.push_back(roofGraphNode);
-
-                   root->addChild(wallsGraphNode);*/
         }
 
 
@@ -318,19 +303,51 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
     };
 
-    // positions all containers
-    glm::vec3 cubePositions[] = {
-        glm::vec3(0.0f,  0.0f,  0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
+    float skyboxVertices[] = {
+        // positions          
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f
     };
+
 
     // positions of the point lights
     glm::vec3 lightsPositions[] = {
@@ -367,14 +384,29 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // skybox VAO
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
+    // load textures
+    // -------------
+    std::vector<std::string> faces
+    {
+         ("res/textures/skybox/right.jpg"),
+         ("res/textures/skybox/left.jpg"),
+         ("res/textures/skybox/top.jpg"),
+         ("res/textures/skybox/bottom.jpg"),
+         ("res/textures/skybox/front.jpg"),
+         ("res/textures/skybox/back.jpg"),
+    };
 
-
-    // load textures (we now use a utility function to keep the code more organized)
-   // -----------------------------------------------------------------------------
-   //unsigned int diffuseMap = loadTexture("res/textures/container2.png");
-   //unsigned int specularMap = loadTexture("res/textures/container2_specular.png");
-
+    unsigned int cubemapTexture = loadCubeMap(faces);
 
 
     int angle = 1;
@@ -423,6 +455,9 @@ int main()
 
     lightingShaderProgram.setInt("texture_diffuse1", 0);
     lightingShaderProgram.setInt("texture_specular1", 1);
+
+    skyboxShaderProgram.Use();
+    skyboxShaderProgram.setInt("skybox", 0);
 
     // render loop
     while (window.isOpen())
@@ -663,7 +698,19 @@ int main()
             glBindVertexArray(0);
         }
 
-
+        // draw skybox as last
+        glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+        skyboxShaderProgram.Use();
+        view = glm::mat4(glm::mat3(camera.GetViewMatrix())); // remove translation from the view matrix
+        skyboxShaderProgram.setMat4("view", view);
+        skyboxShaderProgram.setMat4("projection", projection);
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthFunc(GL_LESS); // set depth function back to default
 
 
         // Render dear imgui into screen
@@ -688,6 +735,35 @@ int main()
     return 0;
 }
 
+unsigned int loadCubeMap(std::vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrComponents;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrComponents, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
